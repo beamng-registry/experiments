16c3f9705e543ef8402d841689b5230ab8d94f28
scx1332 <sieciech@gmail.com>
2026-03-29 20:33:51 +0200

    Fixing brakes and stuff

--- deploy.bat
+++ deploy.bat
@@ -1,16 +1,3 @@
 @echo off
 
-
-del "%destinationZip%" /y 2>nul
-:: --- EXECUTION ---
-echo Compressing %sourceDir% into %destinationZip%...
-"C:\Program Files\7-Zip\7z.exe" a -tzip citybus.zip vehicles
-
-if %ERRORLEVEL% EQU 0 (
-    echo Compression successful!
-
-    :: Move to BeamNG folder
-    move citybus.zip "C:\Users\admin-local\AppData\Local\BeamNG\BeamNG.drive\current\mods"
-) else (
-    echo Compression failed with error code %ERRORLEVEL%.
-)
+wsl rsync -avz ./vehicles/citybus /mnt/c/Users/admin-local/AppData/Local/BeamNG/BeamNG.drive/current/mods/unpacked/citybus/vehicles/
